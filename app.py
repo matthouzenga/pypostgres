6c445bb5ddbc35cdf8a0ada722a6f93f52ededd6
matt <matt@localhost.localdomain>
2021-05-26 15:06:44 -0400

    version 3

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+pUser = os.getenv('database-user')       
+pPassword = os.getenv('database-password')       
+pDatabase = os.getenv('database-name')       
 
 @app.route('/')
 def index():
+    out_string = 'database: ' + pDatabase + 'user: ' + pUser + ' password: ' + pPassword
     return 'Hello, Web App with Python Flask!'
 
 app.run(host='0.0.0.0', port=5000)
