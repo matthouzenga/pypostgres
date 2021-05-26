784381ef31eea8ede94893cd4a7ac2b546385186
matt <matt@localhost.localdomain>
2021-05-26 15:34:22 -0400

    version 4

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from flask import jsonify
 import os
+import psycopg2
 
 app = Flask(__name__)
-pUser = os.getenv('database-user')       
-pPassword = os.getenv('database-password')       
-pDatabase = os.getenv('database-name')       
+pUser = os.getenv('database-user')
+pPassword = os.getenv('database-password')
+pDatabase = os.getenv('database-name')
 
 @app.route('/')
 def index():
-    out_string = 'version 3 - database: ' + pDatabase + 'user: ' + pUser + ' password: ' + pPassword
+    conn = psycopg2.connect(
+       host="postgresql",
+       database=pDatabase,
+       user=pUser,
+       password=pPassword)
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sample')
+
+    conn.close()    
+
+    return_row = cur.fetchone()
+
+    out_string = 'row 1: ' + return_row[0] + ' ' + str(return_row[1])
     return out_string
 
 app.run(host='0.0.0.0', port=5000)
